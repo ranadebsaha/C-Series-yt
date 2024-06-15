e9c8fa2bcbba52558297a883dbf53e6b9eb151f0
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2024-06-15 23:09:11 +0530

    Add files via upload

---
+++ string_size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char arr[]="abcd";
+    int n;
+    // while(arr[n]!='\0'){
+    //     n++;
+    // }
+    n=strlen(arr);
+    printf("%d",n);
+}
